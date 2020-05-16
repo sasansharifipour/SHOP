a675cacbd8392528792388e7ea52a0dcefca432a
sasansharifipour <sasansharifipour@gmail.com>
2020-05-16 11:44:32 +0430

    Add Address Entity

---
+++ DomainClasses/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainClasses
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int CityId { get; set; }
+
+        public City City { get; set; }
+
+        public string Neighbourhood { get; set; }
+
+        public string MainStreet { get; set; }
+
+        public string ByStreet { get; set; }
+
+        public string Alley { get; set; }
+
+        public string Number { get; set; }
+
+
+    }
+}
