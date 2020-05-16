ed01a4547e977292caede9e049eeedb0cf629c2f
sasansharifipour <sasansharifipour@gmail.com>
2020-05-16 11:44:47 +0430

    Add Image Entity

---
+++ DomainClasses/Image.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainClasses
+{
+    public class Image
+    {
+        public int Id { get; set; }
+
+        public string fileName { get; set; }
+    }
+}
