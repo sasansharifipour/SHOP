7daa4c5ce2c7b6d7c1b7164053f29562df974847
sasansharifipour <sasansharifipour@gmail.com>
2020-05-16 11:44:59 +0430

    Add Position Entity

---
+++ DomainClasses/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainClasses
+{
+    public class Position
+    {
+        public int Id { get; set; }
+
+        public double Longitude { get; set; }
+
+        public double Latitude { get; set; }
+    }
+}
